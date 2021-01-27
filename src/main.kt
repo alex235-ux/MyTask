b12ee5e473e19abfc8269420d158f1dcaccd2425
BlackfiremasterX <misharozental@gmail.com>
2021-01-27 10:30:59 +0700

    ITV2

---
+++ src/main.kt
@@ -0,0 +1,37 @@
+fun main()
+{
+    println("Решебник на языке программирования Kotlin по Задачнику Абрамяна.")
+    println("Внутренний синтаксис(Begin1 - b1)")
+    println("Список доступных задач:")
+    println("Begin1")
+//    println("Список доступных задач:")
+//    println("Список доступных задач:")
+    print("Выберите задачу: "); var user_choise = readLine()
+    when(user_choise)
+    {
+        "b1" -> Begin1()
+        else -> print("Такой задачи нет!")
+
+
+
+    }
+}
+
+
+
+fun Begin1()
+{
+
+
+
+
+}
+
+
+fun Begin9()
+{
+
+    ////
+
+}
+
